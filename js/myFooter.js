23e69ae524f553aef4f2790b2ccf38ae1cdb85bd
Lexi <leiqianfengf7928@gmail.com>
2019-03-14 14:57:05 -0700

    Upload to git

---
+++ js/myFooter.js
@@ -0,0 +1,15 @@
+var year = new Date().getFullYear();
+
+var date = `&copy; Lexi Feng ${year}. All Rights Reserved.`;
+
+document.getElementsByTagName('footer')[0].innerHTML = date ;
+//[0] the first tag of footer in the page
+//the varibale date wich will set to update annually 
+
+// var link = "http://google.com";
+// var element = document.createElement("a");
+// element.setAttribute("href", link);
+// element.innerHTML = "your text";
+
+// // and append it to where you'd like it to go:
+// document.body.appendChild(element);
