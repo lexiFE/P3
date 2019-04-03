82c4f9c8f37ead4769c6a97fd34bb4ee45684dc1
Lexi <leiqianfengf7928@gmail.com>
2019-04-03 15:36:03 -0700

    Footer and contact
    footer to have links and contact email is clickable to send email to lexi

--- js/myFooter.js
+++ js/myFooter.js
@@ -2,7 +2,7 @@ var year = new Date().getFullYear();
 
 var date = `&copy; Lexi Feng ${year}. All Rights Reserved.`;
 
-document.getElementsByTagName('footer')[0].innerHTML = date ;
+document.getElementsByTagName('footer')[0].innerHTML += date ;
 //[0] the first tag of footer in the page
 //the varibale date wich will set to update annually 
 
